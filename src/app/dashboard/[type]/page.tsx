0e808e4d9b2eeae9e92c9c62958cee17e049e6e4
nvhoang2003 <nvhoang12082003@gmail.com>
2024-03-16 23:07:43 +0700

    setup project

---
+++ src/app/dashboard/[type]/page.tsx
@@ -0,0 +1,11 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+export default function page() {
+
+    return (
+        <div>
+            <Typography variant="h1">Link use slug params</Typography>
+        </div>
+    )
+}
